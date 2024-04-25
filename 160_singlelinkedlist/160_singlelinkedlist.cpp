9bc46025ed17b6c8ff9092dcbd54ad050b20cf20
abimanyusatriaa <adji3890@gmail.com>
2024-04-25 09:47:48 +0700

    membuat kondisi whille

--- 160_singlelinkedlist/160_singlelinkedlist.cpp
+++ 160_singlelinkedlist/160_singlelinkedlist.cpp
@@ -22,6 +22,22 @@ void addNode() {
 
     if (START == NULL || nim <= START->noMhs) {
         if (START != NULL && nim == START->noMhs)
+        {
+            cout << "NIM sudah ada" << endl;
+            return;
+        }
+
+        nodeBaru->next = START;
+        START = nodeBaru;
+        return;
+    }
+
+    Node* previous = START;
+    Node* current = START;
+
+    while ((current. != NULL) && (NIM >= current->noMhs))
+    {
+
     }
 }
 
